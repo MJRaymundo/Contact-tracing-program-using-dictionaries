0ae892185d621cec3ee119b40c414ce2f02fac56
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-11-12 21:49:48 +0800

    Fixed option 1 error checking, Added dictionary

--- Contact tracing program using dictionaries.py
+++ Contact tracing program using dictionaries.py
@@ -11,6 +11,7 @@ user_input = input("Enter the number of the function you want to use: ")
 # Perform Selected Option 
 ## Option 1: Ask personal data for contact tracing // Full Name, Age, Address, Phone Number, Vaccinated, Positive or negative, Had covid before, Comorbidity
 ### User Input
+#/// Full name
 while True:
     try:
         full_name = str(input("What is your full name?: "))
@@ -19,25 +20,72 @@ while True:
         continue
     else:
         break
-age = int(input("What is your age?: "))
-address = input("What is your address?: ")
-phone_number = int(input("What is your phone number?: "))
-vaccine_status = str(input("Are you vaccinated? (Y/N): "))
-covid_status = str(input("Are you currently positive for convid? (Y/N): "))
-previous_covid_status = str(input("Have you been previously positive for covid? (Y/N): "))
+#/// Age
 while True:
     try:
-        comorbidity = str(input("Do you have any comorbidity (Y/N)?: "))
-        comorbidity.upper  == "Y" or "N"
-    except comorbidity != "Y" or "N":
+        age = int(input("What is your age?: "))
+    except ValueError:
         print("Invalid input.")
         continue
-
     else:
         break
-
-print(comorbidity)
+#/// Address
+while True:
+    try:
+        address = input("What is your address?: ")
+    except ValueError:
+        print("Invalid input.")
+        continue
+    if not address.isalnum:
+        print("Invalid input.")
+    else:
+        break
+#/// Phone number
+while True:
+    try:
+        phone_number = int(input("What is your phone number?: "))
+    except ValueError:
+        print("Invalid input.")
+        continue
+    else:
+        break
+#/// Vaccine status
+while True:
+    vaccine_status = str(input("Are you vaccinated? (Y/N): "))
+    if vaccine_status.upper() not in ['Y', 'N']:
+        print("Wrong")
+        continue
+    else:
+        break
+#/// Covid status
+while True:
+    covid_status = str(input("Are you currently postivie for covid? (Y/N): "))
+    if covid_status.upper() not in ['Y', 'N']:
+        print("Wrong")
+        continue
+    else:
+        break
+#/// Previous Covid Status
+while True:
+    previous_covid_status = str(input("Have you ever had covid? (Y/N): "))
+    if previous_covid_status.upper() not in ['Y', 'N']:
+        print("Wrong")
+        continue
+    else:
+        break
+#/// Comorbidity
+while True:
+    comorbidity = str(input("Do you have any comorbidity? (Y/N): "))
+    if comorbidity.upper() not in ['Y', 'N']:
+        print("Wrong")
+        continue
+    else:
+        break
 ### Use dictionary to store info
+contact_tracing_dictionary = {
+    full_name : {age, address, phone_number, vaccine_status, covid_status, previous_covid_status, comorbidity},
+}
+print(contact_tracing_dictionary)
 ### Use full name as key
 ### The value is another dictionary of personal information
 ## Option 2: Search, ask full name then display the record
