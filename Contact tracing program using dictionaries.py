7560ade9705fd03e72b7392c60e081072e01143d
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-11-12 20:36:42 +0800

    Added input for menu

--- Contact tracing program using dictionaries.py
+++ Contact tracing program using dictionaries.py
@@ -1,11 +1,12 @@
 # Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
 # Display Menu
 print("Menu: ")
-print("     1 -> Add an item. ")
-print("     2 -> Search. ")
-print("     3 -> Exit. ")
+print("      1 -> Add an item. ")
+print("      2 -> Search. ")
+print("      3 -> Exit. ")
 # Allow user to select and item in the menu
 ## User Input
+user_input = input("Enter the number of the function you want to use: ")
 ## Check if valid
 # Perform Selected Option 
 ## Option 1: Ask personal data for contact tracing
