17bbb8f2edd1b7e1f2c584ba0e78f011c5d9018d
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-11-12 20:34:31 +0800

    Added menu

--- Contact tracing program using dictionaries.py
+++ Contact tracing program using dictionaries.py
@@ -1,1 +1,16 @@
 # Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+# Display Menu
+print("Menu: ")
+print("     1 -> Add an item. ")
+print("     2 -> Search. ")
+print("     3 -> Exit. ")
+# Allow user to select and item in the menu
+## User Input
+## Check if valid
+# Perform Selected Option 
+## Option 1: Ask personal data for contact tracing
+### Use dictionary to store info
+### Use full name as key
+### The value is another dictionary of personal information
+## Option 2: Search, ask full name then display the record
+## Option 3: Ask the user if they want to exit or retry
