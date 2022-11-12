908ff19f2bd82febc445851822ab617cbc0db87a
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-11-12 21:31:00 +0800

    Added error checking to option 1 input

--- Contact tracing program using dictionaries.py
+++ Contact tracing program using dictionaries.py
@@ -11,15 +11,32 @@ user_input = input("Enter the number of the function you want to use: ")
 # Perform Selected Option 
 ## Option 1: Ask personal data for contact tracing // Full Name, Age, Address, Phone Number, Vaccinated, Positive or negative, Had covid before, Comorbidity
 ### User Input
-full_name = str(input("What is your full name?: "))
+while True:
+    try:
+        full_name = str(input("What is your full name?: "))
+    except ValueError:
+        print("Invalid input.")
+        continue
+    else:
+        break
 age = int(input("What is your age?: "))
 address = input("What is your address?: ")
 phone_number = int(input("What is your phone number?: "))
 vaccine_status = str(input("Are you vaccinated? (Y/N): "))
 covid_status = str(input("Are you currently positive for convid? (Y/N): "))
 previous_covid_status = str(input("Have you been previously positive for covid? (Y/N): "))
-comorbidity = str(input("Do you have any comorbidity (Y/N)?: "))
-print(full_name, age, address, phone_number, vaccine_status, covid_status, previous_covid_status, comorbidity)
+while True:
+    try:
+        comorbidity = str(input("Do you have any comorbidity (Y/N)?: "))
+        comorbidity.upper  == "Y" or "N"
+    except comorbidity != "Y" or "N":
+        print("Invalid input.")
+        continue
+
+    else:
+        break
+
+print(comorbidity)
 ### Use dictionary to store info
 ### Use full name as key
 ### The value is another dictionary of personal information
